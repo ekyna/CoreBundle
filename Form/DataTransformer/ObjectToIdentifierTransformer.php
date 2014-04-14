1e77a26f26eaa00f221feea997c3a17206ead868
Etienne Dauvergne <contact@ekyna.com>
2014-04-14 21:05:32 +0200

    Translations, DataTransformer, Dependencies

---
+++ Form/DataTransformer/ObjectToIdentifierTransformer.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Ekyna\Bundle\CoreBundle\Form\DataTransformer;
+
+use Doctrine\Common\Persistence\ObjectRepository;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+use Symfony\Component\Form\Exception\UnexpectedTypeException;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+/**
+ * ObjectToIdentifierTransformer
+ *
+ * @author Alexandre Bacco <alexandre.bacco@gmail.com>
+ * @see https://github.com/Sylius/SyliusResourceBundle/blob/master/Form/DataTransformer/ObjectToIdentifierTransformer.php
+ */
+class ObjectToIdentifierTransformer implements DataTransformerInterface
+{
+    /**
+     * Repository
+     *
+     * @var ObjectRepository
+     */
+    protected $repository;
+
+    /**
+     * Identifier
+     *
+     * @var string
+     */
+    protected $identifier;
+
+    /**
+     * Constructor.
+     *
+     * @param ObjectRepository $repository
+     * @param string           $identifier
+     */
+    public function __construct(ObjectRepository $repository, $identifier = 'id')
+    {
+        $this->repository = $repository;
+        $this->identifier = $identifier;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transform($value)
+    {
+        if (!$value) {
+            return null;
+        }
+
+        if (null === $entity = $this->repository->findOneBy(array($this->identifier => $value))) {
+            throw new TransformationFailedException(sprintf(
+                'Object "%s" with identifier "%s"="%s" does not exist.',
+                $this->repository->getClassName(),
+                $this->identifier,
+                $value
+            ));
+        }
+
+        return $entity;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function reverseTransform($value)
+    {
+        if (null === $value) {
+            return '';
+        }
+
+        $class = $this->repository->getClassName();
+
+        if (!$value instanceof $class) {
+            throw new UnexpectedTypeException($value, $class);
+        }
+
+        $accessor = PropertyAccess::createPropertyAccessor();
+
+        return $accessor->getValue($value, $this->identifier);
+    }
+}
