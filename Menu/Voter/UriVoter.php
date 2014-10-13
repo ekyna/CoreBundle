bcba953745854c542b66070d2963e533215a0dae
Etienne Dauvergne <contact@ekyna.com>
2014-10-13 16:46:13 +0200

    KnpMenu Uri Voter

---
+++ Menu/Voter/UriVoter.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Ekyna\Bundle\CoreBundle\Menu\Voter;
+
+use Knp\Menu\ItemInterface;
+use Knp\Menu\Matcher\Voter\VoterInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class UriVoter
+ * @package Ekyna\Bundle\CoreBundle\Menu\Voter
+ * @author Étienne Dauvergne <contact@ekyna.com>
+ */
+class UriVoter implements VoterInterface
+{
+    /**
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * @var string
+     */
+    private $path;
+
+    /**
+     * @var string
+     */
+    private $scriptName;
+
+    /*public function __construct(Request $request = null)
+    {
+        $this->request = $request;
+    }*/
+
+    public function setRequest(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function matchItem(ItemInterface $item)
+    {
+        if (null === $this->request) {
+            return null;
+        }
+
+        $uri = $item->getUri();
+        if (0 === strpos($uri, $this->request->getScriptName())) {
+            $uri = substr($uri, strlen($this->request->getScriptName()));
+        }
+
+        if (1 >= strlen($uri)) {
+            return null;
+        }
+
+        //echo $uri . ' - ' . $this->request->getPathInfo() . '<br>';
+
+        if (preg_match(sprintf('#^%s#', $uri), $this->request->getPathInfo())) {
+            return true;
+        }
+
+        return null;
+    }
+}
