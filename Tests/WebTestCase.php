78542d4862e5f62ea766010c1eea206c18f493b7
Etienne Dauvergne <contact@ekyna.com>
2015-02-25 22:45:47 +0100

    WebTestCase

--- Tests/WebTestCase.php
+++ Tests/WebTestCase.php
@@ -3,7 +3,6 @@
 namespace Ekyna\Bundle\CoreBundle\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseTestCase;
-use Symfony\Component\BrowserKit\Cookie;
 
 /**
  * Class WebTestCase
@@ -23,31 +22,6 @@ abstract class WebTestCase extends BaseTestCase
     public function setUp()
     {
         $this->client = static::createClient();
-    }
-
-    /**
-     * Log the given email
-     * @param string $email
-     */
-    protected function login($email = 'admin@example.org')
-    {
-        $container = $this->client->getContainer();
-
-        $session = $container->get('session');
-        /** @var $userManager \FOS\UserBundle\Doctrine\UserManager */
-        $userManager = $container->get('fos_user.user_manager');
-        /** @var $loginManager \FOS\UserBundle\Security\LoginManager */
-        $loginManager = $container->get('fos_user.security.login_manager');
-        $firewallName = $container->getParameter('fos_user.firewall_name');
-
-        $user = $userManager->findUserBy(array('username' => $email));
-        $loginManager->loginUser($firewallName, $user);
-
-        // save the login token into the session and put it in a cookie
-        $session->set('_security_' . $firewallName, serialize($container->get('security.context')->getToken()));
-        $session->save();
-
-        $this->client->getCookieJar()->set(new Cookie($session->getName(), $session->getId()));
     }
 
     /**
