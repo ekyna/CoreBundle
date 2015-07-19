ee11755495f4f7459472ec7cfa452e14aecb8393
Etienne Dauvergne <contact@ekyna.com>
2015-07-19 19:11:13 +0200

    Address data-role attributes

--- Form/Type/AddressType.php
+++ Form/Type/AddressType.php
@@ -21,22 +21,27 @@ class AddressType extends AbstractType
         $builder
             ->add('street', 'text', array(
                 'label' => 'ekyna_core.field.street',
+                'attr' => array('data-role' => 'street'),
             ))
             ->add('supplement', 'text', array(
                 'label' => 'ekyna_core.field.supplement',
-                'required' => false
+                'required' => false,
             ))
             ->add('postalCode', 'text', array(
                 'label' => 'ekyna_core.field.postal_code',
+                'attr' => array('data-role' => 'postal-code'),
             ))
             ->add('city', 'text', array(
                 'label' => 'ekyna_core.field.city',
+                'attr' => array('data-role' => 'city'),
             ))
             ->add('country', 'country', array(
                 'label' => 'ekyna_core.field.country',
+                'attr' => array('data-role' => 'country'),
             ))
             ->add('state', 'text', array(
                 'label' => 'ekyna_core.field.state',
+                'attr' => array('data-role' => 'state'),
                 'required' => false,
             ))
         ;
