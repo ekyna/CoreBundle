102fdd162673f359e69967b78890d06d2b0ec394
Etienne Dauvergne <contact@ekyna.com>
2016-09-05 14:39:17 +0200

    [Core] Improved entity search.

--- Form/Type/EntitySearchType.php
+++ Form/Type/EntitySearchType.php
@@ -3,13 +3,16 @@
 namespace Ekyna\Bundle\CoreBundle\Form\Type;
 
 use Doctrine\Common\Persistence\ObjectManager;
-use Ekyna\Bundle\CoreBundle\Form\DataTransformer\ObjectToIdentifierTransformer;
+//use Ekyna\Bundle\CoreBundle\Form\DataTransformer\ObjectToIdentifierTransformer;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
+//use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+//use Symfony\Component\Form\Extension\Core\Type\TextType;
+//use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
+//use Symfony\Component\OptionsResolver\Exception\InvalidArgumentException;
+//use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -22,29 +25,29 @@ class EntitySearchType extends AbstractType
     /**
      * @var \Doctrine\Common\Persistence\ObjectManager
      */
-    protected $om;
+    //protected $om;
 
     /**
      * Constructor.
      *
      * @param ObjectManager $om
      */
-    public function __construct(ObjectManager $om)
+    /*public function __construct(ObjectManager $om)
     {
         $this->om = $om;
-    }
+    }*/
 
     /**
      * {@inheritdoc}
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    /*public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $repository = $this->om->getRepository($options['entity']);
         $builder
             ->addModelTransformer(
                 new ObjectToIdentifierTransformer($repository)
             );
-    }
+    }*/
 
     /**
      * {@inheritdoc}
@@ -52,8 +55,9 @@ class EntitySearchType extends AbstractType
     public function finishView(FormView $view, FormInterface $form, array $options)
     {
         $view->vars['attr']['data-search'] = $options['search_route'];
-        $view->vars['attr']['data-find'] = $options['find_route'];
-        $view->vars['attr']['data-clear'] = intval($options['allow_clear']);
+        $view->vars['attr']['data-find']   = $options['find_route'];
+        $view->vars['attr']['data-clear']  = intval($options['allow_clear']);
+        $view->vars['attr']['data-format'] = $options['format_function'];
     }
 
     /**
@@ -61,18 +65,32 @@ class EntitySearchType extends AbstractType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
+        // TODO TODO convert options['choice_label'] to javascript options['format_function']...
+        // TODO Create a choice_loader (DoctrineType) : only current value and submitted value (for validation)
+
         $resolver
             ->setDefaults([
-                'entity'       => null,
                 'search_route' => null,
                 'find_route'   => null,
                 'allow_clear'  => false,
+                'format_function' => null,
             ])
-            ->setRequired(['entity', 'search_route', 'find_route'])
-            ->setAllowedTypes('entity', 'string')
+            ->setRequired(['search_route', 'find_route'])
             ->setAllowedTypes('search_route', 'string')
             ->setAllowedTypes('find_route', 'string')
-            ->setAllowedTypes('allow_clear', 'bool');
+            ->setAllowedTypes('allow_clear', 'bool')
+            ->setAllowedTypes('format_function', ['null', 'string'])
+            /*->setNormalizer('format_function', function(Options $options, $value) {
+                if (0 == strlen($value)) {
+                    if (0 < strlen($options['choice_label'])) {
+                        return 'return data.' . $options['choice_label'] . ';';
+                    }
+
+                    throw new InvalidArgumentException("The option 'format_function' must be defined.");
+                }
+
+                return $value;
+            })*/;
     }
 
     /**
@@ -80,7 +98,7 @@ class EntitySearchType extends AbstractType
      */
     public function getParent()
     {
-        return ChoiceType::class;
+        return EntityType::class;
     }
 
     /**
--- Resources/private/js/form/entity-search.js
+++ Resources/private/js/form/entity-search.js
@@ -14,77 +14,40 @@ define(['jquery', 'routing'], function($, router) {
 
             var $this = $(this);
 
-            var searchUrl = router.generate($this.data('search'));
-            var findUrl = router.generate($this.data('find'));
-            var allowClear = $this.data('clear') == 1;
+            //var findUrl = router.generate($this.data('find'));
+            var formatter = new Function('data', $this.data('format'));
 
             $this.select2({
                 placeholder: 'Rechercher ...',
-                allowClear: allowClear,
+                allowClear: $this.data('clear') == 1,
                 minimumInputLength: 3,
+                templateResult: formatter,
+                //templateSelection: formatter,
                 ajax: {
                     delay: 300,
-                    url: searchUrl,
+                    url: router.generate($this.data('search')),
                     dataType: 'json',
                     data: function (params) {
                         return {
-                            search: params.term, // search term
-                            page: params.page,
+                            query: params.term,
+                            page:  params.page,
                             limit: config.limit
                         };
                     },
                     processResults: function (data, params) {
-                        console.log(data);
-
-                        // parse the results into the format expected by Select2
-                        // since we are using custom formatting functions we do not need to
-                        // alter the remote JSON data, except to indicate that infinite
-                        // scrolling can be used
                         params.page = params.page || 1;
-
                         return {
-                            results: data.items,
+                            results: data.results,
                             pagination: {
                                 more: (params.page * config.limit) < data.total_count
                             }
                         };
                     },
-                    escapeMarkup: function (markup) { return markup; }
+                    escapeMarkup: function (markup) {
+                        return markup;
+                    }
                 }
             });
-
-            /*$this.select2({
-                placeholder: 'Rechercher ...',
-                minimumInputLength: 0,
-                allowClear: allowClear,
-                ajax: {
-                    quietMillis: 300,
-                    url: searchUrl,
-                    dataType: 'jsonp',
-                    data: function (term) { // , page
-                        return {
-                            limit: params.limit,
-                            search: term
-                        };
-                    },
-                    results: function (data) { // , page
-                        return { results: data.results };
-                    }
-                },
-                initSelection : function (element, callback) {
-                    var id = parseInt(element.val());
-                    if(id > 0) {
-                        $.ajax({
-                            url: findUrl,
-                            data: {id: id},
-                            dataType: 'json'
-                        })
-                        .done(function(data) {
-                            callback(data);
-                        });
-                    }
-                }
-            });*/
         });
         return this;
     };
