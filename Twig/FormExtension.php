4f7085790d7222f8a3a6de443c154ec35941031b
Etienne Dauvergne <contact@ekyna.com>
2016-11-23 14:35:36 +0100

    [Core] Added form_help twig function, used for translations.

---
+++ Twig/FormExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Ekyna\Bundle\CoreBundle\Twig;
+
+/**
+ * Class FormExtension
+ * @package Ekyna\Bundle\CoreBundle\Twig
+ * @author  Etienne Dauvergne <contact@ekyna.com>
+ */
+class FormExtension extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('form_help', null, array('node_class' => 'Symfony\Bridge\Twig\Node\SearchAndRenderBlockNode', 'is_safe' => array('html'))),
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'ekyna_core_form';
+    }
+}
