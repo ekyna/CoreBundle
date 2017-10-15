aff4c3e501465d18d262a7d1333fbf93e30f4d03
Etienne Dauvergne <contact@ekyna.com>
2017-10-15 23:13:44 +0200

    [Core] Gsap eassing typescript declarations.

--- Resources/private/typings/gsap.d.ts
+++ Resources/private/typings/gsap.d.ts
@@ -15,3 +15,25 @@ declare module 'gsap/TimelineLite' {
 declare module 'gsap/TimelineMax' {
     export = gsap.TimelineMax;
 }
+
+declare module 'gsap/easing/EasePack' {
+    export = gsap.Ease;
+}
+
+declare var TweenLite: typeof gsap.TweenLite;
+declare var TweenMax: typeof gsap.TweenMax;
+
+declare var Power0: typeof gsap.Linear;
+declare var Power1: typeof gsap.Quad;
+declare var Power2: typeof gsap.Cubic;
+declare var Power3: typeof gsap.Quart;
+declare var Power4: typeof gsap.Quint;
+declare var Back: typeof gsap.Back;
+declare var Elastic: typeof gsap.Elastic;
+declare var Bounce: typeof gsap.Bounce;
+declare var RoughEase: typeof gsap.RoughEase;
+declare var SlowMo: typeof gsap.SlowMo;
+declare var SteppedEase: typeof gsap.SteppedEase;
+declare var Circ: typeof gsap.Circ;
+declare var Expo: typeof gsap.Expo;
+declare var Sine: typeof gsap.Sine;
